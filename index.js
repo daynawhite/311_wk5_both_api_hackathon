e92522a268f6beaa6f90769ea84611b1198bf990
daynawhite <dayna26.2@gmail.com>
2024-01-29 19:18:58 -0600

    fixed merge conflicts

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 4001;
+const salaryRouter = require('./routes/salary')
+const deptRouter = require('./routes/department')
 
 app.use(express.json())
 
@@ -8,8 +10,8 @@ app.get('/', (req, res) => {
     res.send('hello world!');
 })
 
-const salaryRouter = require('./routes/salary')
 app.use('/salaries', salaryRouter)
 app.use('/salary', salaryRouter)
+app.use('/department', deptRouter)
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
